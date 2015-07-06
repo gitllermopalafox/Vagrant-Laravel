27ec319f75ccc483ea54d985bf65994f9ee02044
Guillermo Palafox <palafox.87@gmail.com>
2015-07-05 22:23:33 -0500

    Update nginx.sh

--- provision/components/nginx.sh
+++ provision/components/nginx.sh
@@ -18,8 +18,11 @@ server {
     sendfile off;
      
     location / {
-        try_files \$uri \$uri/ =404;
+        try_files $uri $uri/ /index.php?$query_string;
     }
+ 
+    error_page 404 /404.html;
+    error_page 500 502 503 504 /50x.html;
      
     location ~* \.php {
         include fastcgi_params;
